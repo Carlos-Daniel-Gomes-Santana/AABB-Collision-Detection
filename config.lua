1b6b83e7989626edc02db52fc5cc1cd4695b3d12
Carlos Daniel Gomes Santana <carlos.daniel.programmer@gmail.com>
2023-03-27 00:18:31 -0300

    [MOD] The configuration file doesn't work

--- config.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.window.title = "AABB Collision Detection"
-    t.window.width = 640
-    t.window.height = 640
-    t.window.resizable = false
-    t.window.fullscreen = false
-    t.window.vsync = true
-end
