dd86091b1582897fb790806d3a4f046816d129a1
Carlos Daniel Gomes Santana <cdgomessantana@gmail.com>
2023-03-29 23:25:28 -0300

    [REFACT] Added constraint to border of windows

--- Square.lua
+++ Square.lua
@@ -14,20 +14,29 @@ function Rectangle:update(type)
     local right = type == 2 and "right" or "d"
     local up = type == 2 and "up" or "w"
     local down = type == 2 and "down" or "s"
+
     if love.keyboard.isDown(left) then
-        self.x = self.x - 20
+        if not (self.x == 0) then
+            self.x = self.x - 20
+        end
     end
 
     if love.keyboard.isDown(right) then
-        self.x = self.x + 20
+        if not (self.x + self.width == WINDOW_WIDTH) then
+            self.x = self.x + 20
+        end
     end
 
     if love.keyboard.isDown(up) then
-        self.y = self.y - 20
+        if not (self.y == 0) then
+            self.y = self.y - 20
+        end
     end
 
     if love.keyboard.isDown(down) then
-        self.y = self.y + 20
+        if not (self.y + self.height == WINDOW_HEIGHT) then
+            self.y = self.y + 20
+        end
     end
 end
 
