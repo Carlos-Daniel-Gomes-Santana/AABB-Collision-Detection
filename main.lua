b0f19aea3661650b8170639e6ae492720b4662d2
Carlos Daniel Gomes Santana <cdgomessantana@gmail.com>
2023-03-29 23:28:14 -0300

    [MOD] Resized the Rectangles and transparency of Grid

--- main.lua
+++ main.lua
@@ -15,17 +15,17 @@ love.window.setMode(
 function love.load()
     rectangle_blue = Rectangle(
         0, 0,
-        100, 100,
+        140, 140,
         {0, 0, 1, 1}
     )
     rectangle_green = Rectangle(
         WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-        100, 100,
+        140, 140,
         {0, 1, 0, 1}
     )
     grid = Grid(
         0, 32, 20,
-        {1, 1, 1, 0.5}
+        {1, 1, 1, 0.3}
     )
 end
 
